b283efc69a09d49eeb784a97376f195d63fb1e3e
jmaniii <124217472+jmaniii@users.noreply.github.com>
2024-11-15 22:38:16 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,10 +1,13 @@
 import geo.utils as utils
 
-a,b = 3,4
-c= utils.pythagorass(a,b)
-print('c =',c)
+#calculate the length of c
+
+a, b = 3,4
+c = utils.pythagoras(a,b)
+print("c = ", c)
+
+#calculate the area of circle
 
 r = 10
 area = utils.circle(r)
-print('area =',area)
-print("he;;0p")
+print("area = ", area)
